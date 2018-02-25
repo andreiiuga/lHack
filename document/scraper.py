8fcf5a2358929d45448e8b40d6c67c6d38593bbb
Iulius Curt <iulius.curt@gmail.com>
2018-02-25 15:20:27 +0200

    add scraper

---
+++ document/scraper.py
@@ -0,0 +1,18 @@
+import urllib2
+from bs4 import BeautifulSoup
+from django.utils.http import urlquote
+
+
+SEARCH_TEMPLATE = 'http://eur-lex.europa.eu/search.html?qid=1519556753205&text=%s&scope=EURLEX&type=quick&lang=en'
+
+
+def scrape(query):
+    query_url = SEARCH_TEMPLATE % urlquote(query)
+    print query_url
+    page = urllib2.urlopen(query_url)
+    soup = BeautifulSoup(page, 'html.parser')
+
+    result_titles = soup.findAll('td', attrs={'class': 'publicationTitle'})
+    for title in result_titles:
+        print title.text
+    return [title.text for title in result_titles]
